4ffdecfac69b678ea290eb968b5a2d5d92f855b5
no382001 <102482527+no382001@users.noreply.github.com>
2023-02-16 20:05:36 +0100

    refactored basic environment with add, and searche

--- ground.h
+++ ground.h
@@ -4,6 +4,7 @@
 #include "h/utils.h"
 #include "h/atoms.h"
 
+// -----------------------------------------------------------------------------------------------------
 // proof of concept for using types as results
 
 template <int A, typename... Rest>
@@ -18,6 +19,7 @@ auto constexpr additon(list<integer<A>,Rest...>) {
         static_assert((sizeof...(Rest) > 0), "addition with a single operand");
 		return integer<A + addtion_sub(list<Rest...>{})>{}; // pass cdr to sublayer
 }
+
 using testaddev = decltype(additon(list<integer<2>,integer<3>>{}));
 using testaddevres = decltype(integer<5>{});
 static_assert(is_same_type<testaddev,testaddevres>,"additon of two integers");
@@ -49,3 +51,65 @@ template < typename Proc, typename Args >
 auto constexpr apply(Proc,Args) {
 	return;
 }
+
+// -----------------------------------------------------------------------------------------------------
+// setting up an init-env for eval
+
+// environment table is a list of lists, or pairs
+LIST(environment);
+
+template <typename... columns>
+struct table_entry {};
+
+template <typename Env, typename Entry>
+auto constexpr extend_environment(Entry){
+    return Env::append(make_environment(Entry{}));
+}
+
+struct variable {};
+struct procedure {};
+
+template <typename name>
+struct p_name {};
+
+template <typename... args>
+struct p_args {};
+
+template <typename... body>
+struct p_body {};
+
+
+// search a list of lists, where the car of entry list is the identifier
+// returns void if entry was not found
+// log(n)
+template <typename candidate, typename A, typename... Args>
+auto constexpr table_search(candidate,environment<A,Args...>){
+    using car_of_entry = decltype(IRcar(A{}));
+    if constexpr (is_same_type<candidate,car_of_entry>){
+        return A{};
+	} else if constexpr (sizeof...(Args) > 0) { // if there are still entries left to search
+        return table_search(candidate{},environment<Args...>{});
+    } else {
+        return;
+    }
+}
+
+using env = decltype(make_environment()); // init list
+// (define n 1)
+using n_1 = decltype(table_entry<variable,c_<110>,integer<1>>{});
+// (make-procedure (f n) (+ 1 n))
+using proc_f_n_plus_1_n = decltype(table_entry<procedure,p_name<c_<'f'>>,p_args<c_<'n'>>,p_body<list<plus,integer<1>,c_<'n'>>>>{});
+
+// add entries
+using env1 = decltype(extend_environment<env>(n_1{}));
+using env2 = decltype(extend_environment<env1>(proc_f_n_plus_1_n{}));
+
+using search_expected = decltype(table_entry<variable, c_<110>, integer<1>>{});
+using search_res = decltype(table_search(variable{},env2{}));
+static_assert(is_same_type<search_expected,search_res>, "first entry");
+
+using search_res2 = decltype(table_search(procedure{},env2{})); 
+static_assert(is_same_type<proc_f_n_plus_1_n,search_res2>, "second entry");
+
+using search_res3 = decltype(table_search(int{},env2{}));
+static_assert(is_same_type<void,search_res3>, "non existent entry");
--- h/car_cdr.h
+++ h/car_cdr.h
@@ -2,6 +2,8 @@
 #include "lists.h"
 
 // car and cdr only accepts quoted lists as arguments
+//template <typename T>
+//concept car_cdr_operable_list = is_same_list_t<T,table_entry> || is_quoted(T{}); // howtf do i enforce this in the template argument? ohwell, lets just use list instead of table ernty
 
 template <typename A, typename... Args>
 auto constexpr car_inner(A,Args...){
@@ -30,4 +32,27 @@ auto constexpr cadr(quote<T<Args...>>){
     static_assert(sizeof...(Args) > 0,"cadr on empty list");
     using the_cdr = decltype(cdr(quote<T<Args...>>{}));
     return car(make_quote(the_cdr{}));
+}
+
+
+template <typename A, typename... Args>
+auto constexpr IRcar_inner(A,Args...){
+    return A{};
+}
+
+template <template<class> class T, typename... Args>
+auto constexpr IRcar(T<Args...>){
+    static_assert(sizeof...(Args) > 0,"IRcar on empty list");
+    return IRcar_inner(Args{}...);
+}
+
+template <template<class> class T, typename A, typename... Args>
+auto constexpr IRcdr_inner(A,Args...){
+    return T<Args...>{};
+}
+
+template <template<class> class T, typename... Args>
+auto constexpr IRcdr(T<Args...>){
+    static_assert(sizeof...(Args) > 0,"IRcdr on empty list");
+    return IRcdr_inner<T>(Args{}...);
 }
